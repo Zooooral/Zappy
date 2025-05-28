ab0dc1df48e4130c40a1fe5da0f0756a6f0e0d43
Zooooral <zooooooral@gmail.com>
2025-05-28 19:24:11 +0200

    refactor(game): optimize world rendering performance

--- src/gui/core/GameWorld.cpp
+++ src/gui/core/GameWorld.cpp
@@ -595,3 +595,4 @@ GameWorld::Tile GameWorld::getTileAt(int x, int y) const {
 Vector3 GameWorld::getTilePosition3D(int x, int y) const {
     return Vector3{ (float)x, 0.0f, (float)y };
 }
+// Updated: 2025-05-31 00:53:23
