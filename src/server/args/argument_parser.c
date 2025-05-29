becc1eb22e7186f18a9eb99954313ac346067f98
Zooooral <zooooooral@gmail.com>
2025-05-29 18:49:06 +0200

    feat(args): add argument parsing infrastructure

---
+++ src/server/args/argument_parser.c
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2025
+** src/server/args/argument_parser.c
+** File description:
+** Enhanced command line argument parsing with seed support
+*/
+
+#include "server/server.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+static int validate_config(const server_config_t *config)
+{
+    if (config->port == 0)
+        return -1;
+    if (config->width == 0)
+        return -1;
+    if (config->height == 0)
+        return -1;
+    if (config->max_clients_per_team == 0)
+        return -1;
+    if (config->freq == 0)
+        return -1;
+    if (config->team_count == 0)
+        return -1;
+    if (config->port < 1024 || config->port > 65535)
+        return -1;
+    return 0;
+}
+
+int parse_arguments(int argc, const char **argv, server_config_t *config)
+{
+    memset(config, 0, sizeof(server_config_t));
+    for (int i = 1; i < argc; i++) {
+        if (process_argument(argv, &i, argc, config) == -1)
+            return -1;
+    }
+    return validate_config(config);
+}
