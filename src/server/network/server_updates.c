a61d7d2e84e249b38d69b946d0003dd159c51d09
Zooooral <zooooooral@gmail.com>
2025-05-29 21:33:08 +0200

    feat(network): add server update broadcasting

---
+++ src/server/network/server_updates.c
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2025
+** src/server/network/server_updates.c
+** File description:
+** Server update sending functionality
+*/
+
+#include "server/server.h"
+#include "server/server_updates.h"
+#include <sys/socket.h>
+#include <string.h>
+#include <stdio.h>
+
+void send_position_update(client_t *client, const player_t *player)
+{
+    char response[128];
+
+    snprintf(response, sizeof(response), "ppo #%d %d %d %d\n",
+        player->id, player->x, player->y, player->orientation);
+    send(client->fd, response, strlen(response), 0);
+}
+
+void send_level_update(client_t *client, const player_t *player)
+{
+    char response[128];
+
+    snprintf(response, sizeof(response), "plv #%d %d\n",
+        player->id, player->level);
+    send(client->fd, response, strlen(response), 0);
+}
+
+void send_elevation_start(client_t *client, const player_t *player)
+{
+    char response[128];
+
+    snprintf(response, sizeof(response), "pic %d %d %d #%d\n",
+        player->x, player->y, player->level, player->id);
+    send(client->fd, response, strlen(response), 0);
+}
+
+void send_elevation_end(client_t *client, const player_t *player)
+{
+    char response[128];
+
+    snprintf(response, sizeof(response), "pie %d %d 1\n",
+        player->x, player->y);
+    send(client->fd, response, strlen(response), 0);
+}
