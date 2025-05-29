61997fa621bf9898a9cd5813cf469c137a2cabad
Zooooral <zooooooral@gmail.com>
2025-05-29 21:18:17 +0200

    feat(network): add network event handling interface

---
+++ include/server/network_handler.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** include/server/network_handler.h
+** File description:
+** Network event handling functions
+*/
+
+#ifndef NETWORK_HANDLER_H_
+    #define NETWORK_HANDLER_H_
+
+    #include "server.h"
+
+void network_handle_events(server_t *server, int ready_count);
+
+#endif /* !NETWORK_HANDLER_H_ */
