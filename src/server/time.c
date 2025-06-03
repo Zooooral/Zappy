6d5779360411920bfbafd21158826276bce6cdaf
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-06-03 14:43:42 +0200

    feat: implementation for the new queue

--- src/server/time.c
+++ src/server/time.c
@@ -12,16 +12,49 @@
 
 double get_time_unit(server_t *server)
 {
-    // TODO: Retourner la valeur de l'unité de temps (1/f)
-    return 1.0;
+    if (!server || server->config.freq == 0)
+        return 1.0;
+    return 1.0 / (double)server->config.freq;
 }
 
 void queue_action(client_t *client, action_t *action)
 {
-    // TODO: Ajouter l'action à la file du client
+    if (!client || !action)
+        return;
+    action->next = NULL;
+    if (!client->action_queue_tail) {
+        client->action_queue_head = action;
+        client->action_queue_tail = action;
+    } else {
+        client->action_queue_tail->next = action;
+        client->action_queue_tail = action;
+    }
+    client->action_queue_count++;
 }
 
 void process_actions(server_t *server)
 {
-    // TODO: Parcourir tous les clients et exécuter les actions prêtes
+    double now = get_current_time();
+    client_t *client;
+    action_t *action;
+    action_t *to_free;
+
+    if (!server)
+        return;
+    for (size_t i = 0; i < server->client_count; ++i) {
+        client = &server->clients[i];
+        action = client->action_queue_head;
+        while (action && action->exec_time <= now) {
+            client->action_queue_head = action->next;
+            if (!client->action_queue_head)
+                client->action_queue_tail = NULL;
+            client->action_queue_count--;
+            if (action->callback)
+                action->callback(client, action->data);
+            to_free = action;
+            action = action->next;
+            free(to_free->command);
+            free(to_free);
+        }
+    }
 }
