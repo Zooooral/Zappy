3a97ff383291c5957671185fcbfc9495e739b3c9
Victor Jurgens Mestre <victor.jurgens-mestre@epitech.eu>
2025-06-13 22:46:53 +0200

    fix: time ticking logic improved from the illogical previous one

--- src/server/server_run.c
+++ src/server/server_run.c
@@ -74,30 +74,26 @@ static int handle_poll_events(server_t *server)
     return 0;
 }
 
-static void update_game_and_broadcast(server_t *server, double delta_time,
-    double *broadcast_timer)
+static void update_game_and_broadcast(server_t *server, double delta_time)
 {
     if (server->tick_count % 20 == 0)
         respawn_resources(server->game->map);
     if (server->game)
         game_state_update(server->game, delta_time);
     process_actions(server);
-    *broadcast_timer += delta_time;
-    if (*broadcast_timer >= 0.1) {
-        broadcast_seeder_updates(server);
-        *broadcast_timer = 0.0;
-    }
 }
 
 static void wait_for_next_tick(server_t *server, double delta_time)
 {
-    double target_time = get_time_unit(server);
-    double sleep_time = target_time - delta_time;
+    static double accumulated_time = 0.0;
+    double time_unit = get_time_unit(server);
 
-    if (sleep_time > 0.0) {
-        usleep((sleep_time * 1e6));
-    }
+    accumulated_time += delta_time;
+    if (accumulated_time < time_unit)
+        return;
+    accumulated_time -= time_unit;
     server->tick_count++;
+    update_game_and_broadcast(server, delta_time);
 }
 
 void server_run(server_t *server)
@@ -113,8 +109,9 @@ void server_run(server_t *server)
             break;
         if (handle_poll_events(server) == -1)
             break;
-        update_game_and_broadcast(server, delta_time, &broadcast_timer);
         wait_for_next_tick(server, delta_time);
+        printf("[SERVER] Tick %zu: Delta time: %.3f seconds\n",
+            server->tick_count, delta_time);
     }
     printf("[SERVER] Server shutting down\n");
 }
