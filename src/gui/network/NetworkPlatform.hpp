2e4b303cca6654e9c6f44fff25ff9e2319e7c48d
Zooooral <zooooooral@gmail.com>
2025-06-24 00:09:58 +0200

    feat: added build compatibility of the gui on windows

--- src/gui/network/NetworkPlatform.hpp
+++ src/gui/network/NetworkPlatform.hpp
@@ -9,21 +9,23 @@
 #define NETWORKPLATFORM_HPP_
 
 #ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #define NOGDI
+    #define NOUSER
     #include <winsock2.h>
     #include <ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")
-    
+
+    #undef NOGDI
+    #undef NOUSER
     using socket_t = SOCKET;
     constexpr socket_t INVALID_SOCKET_VALUE = INVALID_SOCKET;
-    
     inline int closeSocket(socket_t sock) { return closesocket(sock); }
     inline int getLastError() { return WSAGetLastError(); }
-    inline void initializeNetworking() { 
+    inline void initializeNetworking() {
         WSADATA wsaData;
         WSAStartup(MAKEWORD(2, 2), &wsaData);
     }
     inline void cleanupNetworking() { WSACleanup(); }
-    
 #else
     #include <sys/socket.h>
     #include <netinet/in.h>
@@ -31,15 +33,14 @@
     #include <unistd.h>
     #include <fcntl.h>
     #include <errno.h>
-    
+
     using socket_t = int;
     constexpr socket_t INVALID_SOCKET_VALUE = -1;
-    
+
     inline int closeSocket(socket_t sock) { return close(sock); }
     inline int getLastError() { return errno; }
     inline void initializeNetworking() { }
     inline void cleanupNetworking() { }
-    
 #endif
 
 #include <string>
