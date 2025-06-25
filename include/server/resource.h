47d232d3dd77c4cbcb1cdbe845279afc3543d156
flav-code <flavcraft28@gmail.com>
2025-06-25 05:18:52 +0200

    feat(server): enhance broadcast and resource management functions

--- include/server/resource.h
+++ include/server/resource.h
@@ -12,6 +12,6 @@
 
 int take_resource(client_t *client, map_t *map, int resource_id);
 int set_resource(client_t *client, map_t *map, int resource_id);
-void respawn_resources(map_t *map);
+void respawn_resources(server_t *server);
 
 #endif // RESOURCE_H
--- src/server/broadcast.c
+++ src/server/broadcast.c
@@ -8,26 +8,60 @@
 #include <stdio.h>
 
 #include "server/broadcast.h"
+#include "server/server_updates.h"
+#include "server/payloads.h"
 
 void broadcast_message(client_t *sender, const char *msg)
 {
     // TODO: Propager le message à tous les joueurs avec direction
 }
 
-void broadcast_message_to_guis(server_t *server, client_t *sender,
-    void (*function)(client_t *, const player_t *))
+void broadcast_message_to_guis(server_t *server, player_t *player,
+    const char *(*function)(client_t *, const player_t *))
 {
-    size_t i;
     client_t *client = NULL;
-    player_t *player = NULL;
+    const char *payload = function(client, player);
 
-    if (!server)
+    if (!server || !player || !function)
         return;
-    for (i = 0; i < server->client_count; ++i) {
+    for (size_t i = 0; i < server->client_count; ++i) {
         client = &server->clients[i];
-        player = player_find_by_id(server, sender->fd);
-        if (client->type == CLIENT_TYPE_GRAPHIC && player != NULL) {
-            function(client, (const player_t *)player);
+        if (client->type == CLIENT_TYPE_GRAPHIC) {
+            send_response(client, payload);
         }
     }
+    free((void *)payload);
+}
+
+void broadcast_tile_to_guis(server_t *server, int x, int y)
+{
+    client_t *client = NULL;
+    char *payload = gui_payload_tile(server, x, y);
+
+    if (!server || !payload)
+        return;
+    for (size_t i = 0; i < server->client_count; ++i) {
+        client = &server->clients[i];
+        if (client->type == CLIENT_TYPE_GRAPHIC) {
+            send_response(client, payload);
+        }
+    }
+    free(payload);
+}
+
+void broadcast_player_resource_update(server_t *server, player_t *player,
+    int resource_id, char *(*function)(const player_t *, int))
+{
+    client_t *client = NULL;
+    char *payload = function(player, resource_id);
+
+    if (!server || !payload)
+        return;
+    for (size_t i = 0; i < server->client_count; ++i) {
+        client = &server->clients[i];
+        if (client->type == CLIENT_TYPE_GRAPHIC) {
+            send_response(client, payload);
+        }
+    }
+    free(payload);
 }
