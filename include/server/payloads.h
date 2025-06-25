b0275fa07361b4e649f3d9abe83c9613eda1eed9
flav-code <flavcraft28@gmail.com>
2025-06-25 18:18:19 +0200

    feat(server): add player death payload and elevation handling functions

--- include/server/payloads.h
+++ include/server/payloads.h
@@ -13,6 +13,8 @@
 const char *gui_payload_new_player(client_t *, const player_t *player);
 const char *gui_payload_position_update(client_t *, const player_t *player);
 const char *gui_payload_level_update(client_t *, const player_t *player);
+const char *gui_payload_player_death(client_t *, const player_t *player);
+
 const char *gui_payload_elevation_start(client_t *, const player_t *player);
 const char *gui_payload_elevation_end(client_t *, const player_t *player);
 
