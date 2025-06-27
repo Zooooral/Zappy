86e2395473f78f5e53f9f3888aa390791dadf82d
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-06-27 16:29:46 +0200

    chore(server): remove some comments

--- src/server/lifecycle.c
+++ src/server/lifecycle.c
@@ -11,20 +11,16 @@
 
 void consume_food(client_t *client)
 {
-    // TODO: Consommer de la nourriture, mort si 0
 }
 
 void player_die(client_t *client)
 {
-    // TODO: Gérer la mort du joueur
 }
 
 void fork_player(client_t *client, map_t *map)
 {
-    // TODO: Créer un œuf
 }
 
 void hatch_egg(void *egg_data)
 {
-    // TODO: Faire éclore un œuf
 }
