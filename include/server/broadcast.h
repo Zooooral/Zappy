0cfbaddb9f0dddeeeb3d351415c17b970725db25
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-06-28 19:11:01 +0200

    feat(server): new broadcast functions

--- include/server/broadcast.h
+++ include/server/broadcast.h
@@ -17,4 +17,17 @@ void broadcast_tile_to_guis(server_t *server, int x, int y);
 void broadcast_player_resource_update(server_t *server, player_t *player,
     int resource_id, char *(*function)(const player_t *, int));
 
+/* Player-related broadcasts */
+void broadcast_player_death(server_t *server, player_t *player);
+
+/* Game state broadcasts */
+void broadcast_game_end(server_t *server, int winning_team_id);
+void broadcast_time_unit(server_t *server);
+void broadcast_time_unit_modified(server_t *server, int time_unit);
+
+/* Egg-related broadcasts */
+void broadcast_egg_laid(server_t *server, int egg_id, player_t *player);
+void broadcast_egg_hatched(server_t *server, int egg_id);
+void broadcast_egg_died(server_t *server, int egg_id);
+
 #endif // BROADCAST_H
