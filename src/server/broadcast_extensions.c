d053cdd1f8d4d02a438c1ef62f5821a33ffa1d3c
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-06-28 20:12:38 +0200

    refactor(server): move some broadcast function to another file

--- src/server/broadcast_extensions.c
+++ src/server/broadcast_extensions.c
@@ -12,39 +12,6 @@
 #include "server/payloads.h"
 #include "server/server.h"
 
-void broadcast_player_death(server_t *server, player_t *player)
-{
-    char *payload = gui_payload_pdi(NULL, player);
-
-    if (!server || !player || !payload)
-        return;
-    broadcast_string_message_to_guis(server, payload);
-    free(payload);
-}
-
-void broadcast_game_end(server_t *server, int winning_team_id)
-{
-    char *payload = gui_payload_game_end(winning_team_id);
-
-    if (!server || !payload)
-        return;
-    broadcast_string_message_to_guis(server, payload);
-    free(payload);
-}
-
-void broadcast_time_unit(server_t *server)
-{
-    char *payload = NULL;
-
-    if (!server)
-        return;
-    payload = gui_payload_time_unit_get(server->config.freq);
-    if (payload) {
-        broadcast_string_message_to_guis(server, payload);
-        free(payload);
-    }
-}
-
 void broadcast_time_unit_modified(server_t *server, int time_unit)
 {
     char *payload = gui_payload_time_unit_set(time_unit);
