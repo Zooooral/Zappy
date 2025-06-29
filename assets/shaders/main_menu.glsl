8b9d365fe1b83285ff8722493c3aa9ef1c787ec5
flav-code <flavcraft28@gmail.com>
2025-06-29 23:25:16 +0200

    fix(shaders): improve code formatting in main_menu.glsl and water_turbulence.glsl

--- assets/shaders/main_menu.glsl
+++ assets/shaders/main_menu.glsl
@@ -13,16 +13,16 @@ void mainImage(out vec4 o, vec2 u)
     vec4 z = o = vec4(1, 2, 3, 0);
 
     for (float a = .5, t = iTime, i = 0.0;
-         ++i < 19.;
-         o += (1. + cos(z+t))
+        ++i < 19.;
+        o += (1. + cos(z+t))
             / length((1.+i*dot(v,v))
-                   * sin(1.5*u/(.5-dot(u,u)) - 9.*u.yx + t))
-         ) {
+                * sin(1.5*u/(.5-dot(u,u)) - 9.*u.yx + t))
+        ) {
         v = cos(++t - 7.*u*pow(a += .03, i)) - 5.*u;
         u += tanh(40. * dot(u *= mat2(cos(i + .02*t - vec4(0,11,33,0))),u)
-                      * cos(1e2*u.yx + t)) / 2e2
-           + .2 * a * u
-           + cos(4./exp(dot(o,o)/1e2) + t) / 3e2;
+            * cos(1e2*u.yx + t)) / 2e2
+            + .2 * a * u
+            + cos(4./exp(dot(o,o)/1e2) + t) / 3e2;
     }
 
     o = 25.6 / (min(o, 13.) + 164. / o) - dot(u, u) / 250.;
